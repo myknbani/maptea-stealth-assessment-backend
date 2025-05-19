d6f69d05b27b69ff777b0ec633024e4f403b647d
Richard Michael Coo <richard.michael.coo@gmail.com>
2025-05-19 10:19:41 +0800

    refactor: clean up dummy model and resolver after experiment

--- src/app.resolver.ts
+++ src/app.resolver.ts
@@ -1,28 +1,12 @@
 // hello.resolver.ts
 import { Query, Resolver } from '@nestjs/graphql';
-import { Foo } from './foo.model';
 
 @Resolver()
 export class AppResolver {
-  /**
-   * This query returns a simple string -- "Hello, world!".
-   *
-   * @returns A string "Hello, world!".
-   */
   @Query(() => String, {
     description: 'A simple hello world query',
   })
   hello() {
     return 'Hello, world!';
-  }
-
-  /**
-   * This query returns a Foo object with id 1 and name "Hello, world!".
-   *
-   * @returns A Foo object with id 1 and name "Hello, world!".
-   */
-  @Query(() => Foo)
-  getFoo(): Foo {
-    return new Foo(1, 'Hello, world!');
   }
 }
