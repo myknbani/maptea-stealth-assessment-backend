02fd4585938fd63ec7a5daaa97e86c1e023242f8
Richard Michael Coo <richard.michael.coo@gmail.com>
2025-05-20 10:21:16 +0800

    feat: add input for the register mutation

---
+++ src/leads/models/register-lead.input.ts
@@ -0,0 +1,12 @@
+import { InputType, OmitType } from '@nestjs/graphql';
+import { Lead } from './lead.entity';
+
+@InputType()
+export class RegisterLeadInput extends OmitType(Lead, [
+  'id',
+  'createdAt',
+  'updatedAt',
+  'servicesInterests',
+] as const) {
+  servicesInterests: string[];
+}
