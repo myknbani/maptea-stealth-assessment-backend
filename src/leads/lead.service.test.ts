8afe6320a1697c801d57e97d390fe568732d1f56
Richard Michael Coo <richard.michael.coo@gmail.com>
2025-05-20 11:46:22 +0800

    feat: create LeadService class with a method to register leads

---
+++ src/leads/lead.service.test.ts
@@ -0,0 +1,92 @@
+import { Test } from '@nestjs/testing';
+import { RegisterLeadInput } from './models/register-lead.input';
+import { LeadService } from './lead.service';
+import { LeadRepository } from './lead.repository';
+import { ServiceTypeRepository } from './service-type.repository';
+import { Collection, EntityManager } from '@mikro-orm/postgresql';
+import { Lead } from './models/lead.entity';
+import { ServiceType } from './models/dining-service-type.entity';
+import { createMock } from '@golevelup/ts-jest';
+
+describe('LeadService', () => {
+  describe('#createLead', () => {
+    let leadService: LeadService;
+    let leadRepository: LeadRepository;
+    let serviceTypeRepository: ServiceTypeRepository;
+    let entityManager: EntityManager;
+
+    const mockServices = [
+      new ServiceType({
+        id: 1,
+        name: 'pick-up',
+        createdAt: new Date('2025-05-05'),
+        updatedAt: new Date('2025-05-05'),
+      }),
+      new ServiceType({
+        id: 2,
+        name: 'delivery',
+        createdAt: new Date('2025-05-05'),
+        updatedAt: new Date('2025-05-05'),
+      }),
+    ];
+
+    beforeEach(async () => {
+      const mockMOdule = await Test.createTestingModule({
+        providers: [LeadService],
+      })
+        .useMocker(createMock)
+        .compile();
+
+      leadRepository = mockMOdule.get(LeadRepository);
+      serviceTypeRepository = mockMOdule.get(ServiceTypeRepository);
+      entityManager = mockMOdule.get(EntityManager);
+
+      leadService = mockMOdule.get(LeadService);
+    });
+
+    it('creates a lead with the given input', async () => {
+      // Arrange
+      const lead = new RegisterLeadInput({
+        email: 'mike@gmail.com',
+        fullPhoneNumber: '639294584946',
+        name: 'Mike',
+        postCode: '1234',
+        servicesInterests: ['pick-up', 'delivery'],
+      });
+
+      const { servicesInterests, ...profile } = lead;
+
+      jest.spyOn(leadRepository, 'create').mockReturnValue(
+        new Lead({
+          ...profile,
+          id: 1,
+          createdAt: new Date('2025-05-05'),
+          updatedAt: new Date('2025-05-05'),
+          servicesInterests: new Collection<ServiceType>([]),
+        }),
+      );
+
+      jest.spyOn(serviceTypeRepository, 'find').mockResolvedValue(mockServices);
+
+      // Act
+      const result = await leadService.createLead(lead);
+
+      // Assert
+      expect(result).toEqual(
+        new Lead({
+          id: 1,
+          ...profile,
+          createdAt: new Date('2025-05-05'),
+          updatedAt: new Date('2025-05-05'),
+          servicesInterests: new Collection(mockServices),
+        }),
+      );
+
+      expect(leadRepository.create).toHaveBeenCalledWith(profile);
+      expect(serviceTypeRepository.find).toHaveBeenCalledWith({
+        name: { $in: servicesInterests },
+      });
+      expect(entityManager.flush).toHaveBeenCalled();
+    });
+  });
+});
---
+++ src/leads/lead.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@nestjs/common';
+import { LeadRepository } from './lead.repository';
+import { ServiceTypeRepository } from './service-type.repository';
+import { Lead } from './models/lead.entity';
+import { EntityManager } from '@mikro-orm/postgresql';
+import { RegisterLeadInput } from './models/register-lead.input';
+
+@Injectable()
+export class LeadService {
+  constructor(
+    private readonly leadRepository: LeadRepository,
+    private readonly serviceTypeRepository: ServiceTypeRepository,
+    private readonly entityManager: EntityManager,
+  ) {}
+
+  async createLead(leadInput: RegisterLeadInput): Promise<Lead> {
+    const { servicesInterests, ...profile } = leadInput;
+    const lead = this.leadRepository.create(profile as Lead);
+
+    const serviceTypes = await this.serviceTypeRepository.find({
+      name: { $in: servicesInterests },
+    });
+    lead.servicesInterests.set(serviceTypes);
+
+    await this.entityManager.flush();
+    return lead;
+  }
+}
