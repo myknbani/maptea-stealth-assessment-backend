c480b8f07c7596866ef34a7966d59b5710b737b4
Richard Michael Coo <richard.michael.coo@gmail.com>
2025-05-21 08:42:16 +0800

    fix: only seed when empty, since seeders run on backend start

--- src/seeds/service-type.seeder.ts
+++ src/seeds/service-type.seeder.ts
@@ -3,8 +3,13 @@ import { Seeder } from '@mikro-orm/seeder';
 import { ServiceType } from '../leads/models/service-type.entity';
 
 export class ServiceTypeSeeder extends Seeder {
-  run(entityManager: EntityManager) {
+  async run(entityManager: EntityManager) {
     const repository = entityManager.getRepository(ServiceType);
+
+    if ((await repository.count()) > 0) {
+      return;
+    }
+
     repository.create(new ServiceType({ name: 'pick-up' }));
     repository.create(new ServiceType({ name: 'delivery' }));
     repository.create(new ServiceType({ name: 'payment' }));
